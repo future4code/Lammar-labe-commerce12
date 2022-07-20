1e09ada9bd9efbee2bedfd5d90e60e3a09ec8297
Cainã Perotti Barbosa <caiperotti@gmail.com>
2022-07-19 21:49:31 -0300

    Renderização da Lista de Produtos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Cards} from "./componentes/Cards"
-import { Filter } from './components/Filter';
+import { Filter } from './componentes/Filter';
+import { arrayDeProdutos } from './componentes/Produtos'
+
 
 function App() {
   const [inputValMin, setInputValMin] = useState(-Infinity)
   const [inputValMax, setInputValMax] = useState(Infinity)
   const [query, setQuery] = useState("")
+  const array = [...arrayDeProdutos]
+
+  function callback (product) {
+    return <Cards key = {product.id} photo = {product.photo} price = {product.price} name = {product.name} />
+  }
+
+  function filtroMin (product){
+    return product.price >= inputValMin
+  }
+  function filtroMax (product){
+    return product.price <= inputValMax
+  }
+
+  function filtroBusca (product){
+    return product.name.includes(query)
+  }
+  
+  const listaFiltrada = array.filter(filtroMin).filter(filtroMax).filter(filtroBusca).map(callback)
+
+
+  
   return (
     <div className="App">
       <Filter setInputValMax = {setInputValMax} setInputValMin = {setInputValMin} setQuery = {setQuery}></Filter>
+      {listaFiltrada}
     </div>
   );
 }
--- src/componentes/Cards.js
+++ src/componentes/Cards.js
@@ -5,9 +5,9 @@ export function Cards (props){
 
     return (
         <div>
-           <img src= {props.imagem}/>
-           <h3>{props.nome}</h3>
-           <h4>R${props.preco}</h4>
+           <img src= {props.photo}/>
+           <h3>{props.name}</h3>
+           <h4>R${props.price}</h4>
            <button>Adicionar ao carrinho</button>
         </div>
 
