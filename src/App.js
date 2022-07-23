30d75c90c877b73fcbdbe6c0d23fed13071876ea
Cainã Perotti Barbosa <caiperotti@gmail.com>
2022-07-23 10:33:03 -0300

    Carrinho sem Estilização

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import {Cards} from "./componentes/Cards"
 import { Filter } from './componentes/Filter';
 import { arrayDeProdutos } from './componentes/Produtos'
+import {CarrinhoDeProdutos} from "./componentes/CarrinhoDeProdutos"
 
 
 function App() {
@@ -12,9 +13,15 @@ function App() {
   const [inputValMax, setInputValMax] = useState(Infinity)
   const [query, setQuery] = useState("")
   const array = [...arrayDeProdutos]
+  const [listaDeProdutos, setListaDeProdutos] = useState([...arrayDeProdutos])
+  
+
+
+
+
 
   function callback (product) {
-    return <Cards key = {product.id} photo = {product.photo} price = {product.price} name = {product.name} />
+    return <Cards addCart = {addCart} key = {product.id} id = {product.id} photo = {product.photo} price = {product.price} name = {product.name} />
   }
 
   function filtroMin (product){
@@ -30,12 +37,51 @@ function App() {
   
   const listaFiltrada = array.filter(filtroMin).filter(filtroMax).filter(filtroBusca).map(callback)
 
+  function filtroQuantidade (value) { 
+    return value.quantity >= 1;     
+  }
+
+  function callbackCarrinho (cartProduct){
+    return <CarrinhoDeProdutos removeCart = {removeCart} key = {cartProduct.id} id = {cartProduct.id} photo = {cartProduct.photo} quantity = {cartProduct.quantity} price = {cartProduct.price * cartProduct.quantity} name = {cartProduct.name} />
+  }
+
+  const produtosCarrinho = listaDeProdutos.filter(filtroQuantidade).map(callbackCarrinho)
+
+    function addCart (id) {
+    setListaDeProdutos(listaDeProdutos.map(value => 
+      { 
+        if (value.id === id) {
+          return {...value, quantity : (value.quantity + 1)}
+        }
+        return {...value}
+      }
+      ))
+  }
+
+  function removeCart (id) {
+    setListaDeProdutos(listaDeProdutos.map(value => 
+      { 
+        if (value.id === id) {
+          return {...value, quantity : (value.quantity - 1)}
+        }
+        return {...value}
+      }
+      ))
+
+  }
+
+
+
+
+
+    
 
   
   return (
     <div className="App">
-      <Filter setInputValMax = {setInputValMax} setInputValMin = {setInputValMin} setQuery = {setQuery}></Filter>
+      <Filter setInputValMax = {setInputValMax} setInputValMin = {setInputValMin} setQuery = {setQuery} array = {array}></Filter>
       {listaFiltrada}
+      {produtosCarrinho}
     </div>
   );
 }
--- src/componentes/Produtos.js
+++ src/componentes/Produtos.js
@@ -7,49 +7,56 @@ export const arrayDeProdutos = [
         id : 1,
         name : "Viagem para Lua",
         price : 100.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 2,
         name : "Viagem para Marte",
         price : 200.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 3,
         name : "Viagem para Júpiter",
         price : 300.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 4,
         name : "Viagem para Saturno",
         price : 400.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 5,
         name : "Viagem para Urano",
         price : 500.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 6,
         name : "Viagem para Netuno",
         price : 600.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300",
+        quantity : 0
     },
 
     {
         id : 7,
         name : "Viagem para Plutão",
         price : 700.00,
-        photo: "https://picsum.photos/200/300"
+        photo: "https://picsum.photos/200/300", 
+        quantity : 0
     },
 
 ]
